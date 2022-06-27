254492cd4bf8bef458ac508d98ddbea3a2cfc16f
KennyOlsen <106634393+KennyOlsen@users.noreply.github.com>
2022-06-27 11:00:58 -0600

    Data fetched

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+const API_URL = "https://fakestoreapi.com";
+
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        currentPage: 'welcome',
+        storeList: []
+    },
+    methods: {
+        getStoreData: async function () {
+            let response = await fetch(API_URL + '/products');
+            let answer = await response.json();
+            this.storeList = answer;
+        },
+        toStorePage: function () {
+            this.currentPage = 'store';
+        }
+    }
 });
+
+Vue.component('welcome-page', {
+    data: function () {
+        return {}
+    },
+    methods: {},
+    template: `
+
+    `
+})
